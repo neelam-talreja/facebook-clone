7268892223f82c43ddc0da024f9650147fd47089
Neelam <neelam.talreja@bdiplus.com>
2020-09-17 07:31:04 -0400

    Updates

---
+++ precache-manifest.2abdbe43cca8074867a7c615cffa574e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f721e1e92e96276212dc46c9905c70d9",
+    "url": "/facebook-clone/index.html"
+  },
+  {
+    "revision": "e193884a55668636a59e",
+    "url": "/facebook-clone/static/css/main.06bbe13c.chunk.css"
+  },
+  {
+    "revision": "a21699da129e9ade4ac5",
+    "url": "/facebook-clone/static/js/2.cf25dbf0.chunk.js"
+  },
+  {
+    "revision": "14c54f8d621b663941f58f815237ec03",
+    "url": "/facebook-clone/static/js/2.cf25dbf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e193884a55668636a59e",
+    "url": "/facebook-clone/static/js/main.6aeac608.chunk.js"
+  },
+  {
+    "revision": "8d9db31eeef904c1dc55",
+    "url": "/facebook-clone/static/js/runtime-main.389e76a3.js"
+  }
+]);
